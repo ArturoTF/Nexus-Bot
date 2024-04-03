2b9ebb5a64a131a43effba3b5be172893c984283
Arturo Troyano Freire <53167443+ArturoTF@users.noreply.github.com>
2024-04-03 09:37:53 +0200

    Update index.py

--- index.py
+++ index.py
@@ -7,9 +7,6 @@ from traductor.traductor import bot
 intents = discord.Intents.default()
 intents.messages = True
 
-# Token de tu bot proporcionado por Discord Developer Portal
-BOT_TOKEN = 'MTEzNTkwNDg5NzIzODMwMjcyMA.Gw0cpn.bSMuYM6cwhAjmd7TQ9p4K8XmBPWD80goDO78Mk'
-
 # Variable global para almacenar la ID del canal
 canal_id_eventos = None
 
