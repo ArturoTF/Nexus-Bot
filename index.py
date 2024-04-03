4ca7fddfa463f6ff8c5186916726dc51fc8093d1
Arturo Troyano Freire <53167443+ArturoTF@users.noreply.github.com>
2024-04-03 09:34:51 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 # index.py
 import discord
+import os
 from discord.ext import commands  # Agrega esta línea
 from traductor.traductor import bot
 
@@ -60,5 +61,5 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         pass
     # Puedes agregar más manejo de errores según sea necesario
-
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot.run(BOT_TOKEN)
