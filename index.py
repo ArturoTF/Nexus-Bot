4aa46e3a21b820c181a8b27e5fd9ee40894f77ef
Arturo Troyano Freire <53167443+ArturoTF@users.noreply.github.com>
2024-04-03 09:55:15 +0200

    Update index.py

--- index.py
+++ index.py
@@ -4,9 +4,16 @@ import os
 from discord.ext import commands  # Agrega esta línea
 from traductor.traductor import bot
 from commands import commands
+from discord_slash import SlashCommand  # Esto ahora viene de una extensión compatible
+import commands.commands as commands_module
 
 intents = discord.Intents.default()
 intents.messages = True
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+slash = SlashCommand(bot, sync_commands=True)
+
+commands_module.register_commands(slash)
 
 # Variable global para almacenar la ID del canal
 canal_id_eventos = None
