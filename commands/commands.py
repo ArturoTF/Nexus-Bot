8620fb8013efb2a0c5abba5a7fd8327c584e28aa
Arturo Troyano Freire <53167443+ArturoTF@users.noreply.github.com>
2024-04-03 09:55:24 +0200

    Update commands.py

--- commands/commands.py
+++ commands/commands.py
@@ -1,1 +1,20 @@
+from discord_slash.utils.manage_commands import create_option, create_choice
+
+def register_commands(slash):
+    @slash.slash(
+        name="ceo",
+        description="Muestra quién es el CEO",
+        options=[]
+    )
+    async def ceo(ctx):
+        await ctx.send("El CEO es ArturoTF")
+
+    @slash.slash(
+        name="languages",
+        description="Muestra los idiomas disponibles para traducción",
+        options=[]
+    )
+    async def languages(ctx):
+        languages = "🇬🇧, 🇪🇸, 🇩🇪, 🇷🇺, 🇵🇹, 🇻🇳, 🇨🇳, 🇮🇹, 🇺🇸, 🇵🇱"
+        await ctx.send(f"Idiomas disponibles para traducción: {languages}")
 
