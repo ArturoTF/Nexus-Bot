cbbb5e75dd27928c7f79ef20864d85f8e09e62a0
Arturo Troyano Freire <53167443+ArturoTF@users.noreply.github.com>
2024-04-04 08:20:43 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NEXUS-BOT",
+    version="0.1.0",
+    author="ArturoTF",
+    author_email="arturotroyano@gmail.com",
+    description="Nexus Bot en desarrollo.",
+    packages=find_packages(),
+    install_requires=[
+        "py-cord==2.0.0b1",
+        "pandas==2.0.2",
+        "googletrans==4.0.0-rc1",
+        "google-cloud-translate==1.3.1",
+        "google-cloud-texttospeech==0.2.0",
+    ],
+)
