3b900dde358b5f0c9b38d01780eaa817d0d42249
Arturo Troyano Freire <53167443+ArturoTF@users.noreply.github.com>
2024-04-12 09:59:06 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,14 +1,11 @@
 import discord
 import os
-<<<<<<< HEAD
 from traductor.traductor import bot  # Asume que este es el bot ya inicializado
 from discord_slash import SlashCommand  # Esto ahora viene de una extensión compatible
 from bot_commands.basicCommands import register_commands  # Asume que has renombrado tu módulo a bot_commands y tiene una función register_commands
-=======
 from discord.ext import commands
 from traductor.traductor import bot  # Asegúrate de que esta importación se hace correctamente
 from bot_commands.commands import register_commands  # Función ajustada para py-cord
->>>>>>> cbbb5e75dd27928c7f79ef20864d85f8e09e62a0
 
 # No es necesario inicializar 'bot' de nuevo si ya lo has hecho en traductor.traductor
 # Sólo asegúrate de que 'intents' estén correctamente configurados allí
