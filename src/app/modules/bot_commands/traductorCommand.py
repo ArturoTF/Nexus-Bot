c4b6f03f648f59867291d5742baea386438961de
ArturoTF <arturotroyano@gmail.com>
2024-05-14 22:53:17 +0200

    fix

--- src/app/modules/bot_commands/traductorCommand.py
+++ src/app/modules/bot_commands/traductorCommand.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from googletrans import Translator
-from discord.commands import slash_command
+from discord.commands import slash_command, Option
 from ...environments.utils import get_user_language # Importar desde utils
 from ...environments.logging import safe_log
 
@@ -10,21 +10,15 @@ class TraductorCommands(commands.Cog):
         self.bot = bot
         self.translator = Translator()
 
-    @slash_command(name="translate", description="Traduce el mensaje respondido al idioma establecido o seleccionado.")
-    async def translate(self, ctx, language: discord.Option(str, "Elige un idioma", required=False)):
-        # Recuperar el mensaje respondido
-        reference = ctx.message.reference
-        if reference and isinstance(reference.resolved, discord.Message):
-            original_message = reference.resolved.content
-            # Determinar el idioma de destino
-            if not language:
-                language = await get_user_language(ctx.author.id)
-            # Traducir el mensaje
-            translated_message = self.translator.translate(original_message, dest=language).text
-            # Enviar el mensaje traducido
-            await ctx.respond(translated_message)
-        else:
-            await ctx.respond("Por favor, responde a un mensaje para traducir.")
+    @slash_command(name="translate", description="Traduce el mensaje al idioma establecido o seleccionado.")
+    async def translate(self, ctx, message: Option(str, "Introduce el mensaje que quieres traducir"), language: Option(str, "Elige un idioma", required=False)):
+        # Determinar el idioma de destino
+        if not language:
+            language = await get_user_language(ctx.author.id)
+        # Traducir el mensaje
+        translated_message = self.translator.translate(message, dest=language).text
+        # Enviar el mensaje traducido
+        await ctx.respond(translated_message)
 
 def setup(bot):
     bot.add_cog(TraductorCommands(bot))
