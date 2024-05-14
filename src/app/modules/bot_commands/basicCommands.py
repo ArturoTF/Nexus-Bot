97c0e7adfc353b52c9d41dbf47f25bb712c59ba9
ArturoTF <arturotroyano@gmail.com>
2024-05-14 16:56:49 +0200

    fix

--- src/app/modules/bot_commands/basicCommands.py
+++ src/app/modules/bot_commands/basicCommands.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from discord.commands import slash_command, Option, OptionChoice
 import discord
+import mysql.connector
 from ...environments.utils import emoji_flags  # Importar desde utils
 from ...environments.connection import create_connection, close_connection
 from ...environments.logging import safe_log
@@ -16,7 +17,6 @@ def register_commands(bot):
 
     @bot.slash_command(name="languages", description="Muestra los idiomas disponibles para traducción")
     async def languages(ctx):
-        # Usar los emojis directamente
         language_list = ', '.join(emoji_flags.keys())
         connection = create_connection()
         if connection:
@@ -28,21 +28,20 @@ def register_commands(bot):
     async def setlanguage(ctx, idioma: discord.Option(str, "Elige tu idioma", choices=[OptionChoice(name=f"{flag} {code.upper()}", value=code) for flag, code in emoji_flags.items()])):
         user_name = ctx.author.name
         connection = create_connection()
-        try:
-            if not connection:
-                raise Exception("Failed to connect to database")
-            cursor = connection.cursor()
-            cursor.execute(
-                "INSERT INTO usuarios_idioma (name, idioma) VALUES (%s, %s) ON DUPLICATE KEY UPDATE idioma = VALUES(idioma)",
-                (user_name, idioma)
-            )
-            connection.commit()
-            await ctx.respond(f"{ctx.author.mention}, tu idioma se ha establecido a {emoji_flags[idioma]}")
-            safe_log(connection, "INFO", f"Idioma actualizado para {user_name} a {idioma}", "setlanguage")
-        except Exception as e:
-            if connection:
+        if connection:
+            try:
+                cursor = connection.cursor()
+                cursor.execute(
+                    "INSERT INTO usuarios_idioma (name, idioma) VALUES (%s, %s) ON DUPLICATE KEY UPDATE idioma = VALUES(idioma)",
+                    (user_name, idioma)
+                )
+                connection.commit()
+                await ctx.respond(f"{ctx.author.mention}, tu idioma se ha establecido a {emoji_flags[idioma]}")
+                safe_log(connection, "INFO", f"Idioma actualizado para {user_name} a {idioma}", "setlanguage")
+            except mysql.connector.Error as e:
                 safe_log(connection, "ERROR", f"Error en setlanguage: {e}", "setlanguage")
-            await ctx.respond("Error al procesar tu solicitud. Por favor, inténtalo de nuevo.")
-        finally:
-            if connection:
+                await ctx.respond("Error al procesar tu solicitud. Por favor, inténtalo de nuevo.")
+            finally:
                 close_connection(connection)
+        else:
+            await ctx.respond("Error al conectar con la base de datos.")
