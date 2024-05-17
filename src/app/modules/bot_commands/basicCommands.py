b12f548f6e1279da11ee71f66182a35cb93a2724
ArturoTF <arturotroyano@gmail.com>
2024-05-17 17:55:08 +0200

    fix idUser

--- src/app/modules/bot_commands/basicCommands.py
+++ src/app/modules/bot_commands/basicCommands.py
@@ -31,12 +31,13 @@ class BasicCommands(commands.Cog):
     async def setlanguage(self, ctx, idioma: Option(str, "Elige tu idioma", choices=[OptionChoice(name=f"{flag} {code.upper()}", value=code) for flag, code in emoji_flags.items()])):
         user_name = ctx.author.name
         connection = create_connection()
+        user_id = ctx.author.id
         if connection:
             try:
                 cursor = connection.cursor()
                 cursor.execute(
-                    "INSERT INTO usuarios_idioma (name, idioma) VALUES (%s, %s) ON DUPLICATE KEY UPDATE idioma = VALUES(idioma)",
-                    (user_name, idioma)
+                    "INSERT INTO usuarios_idioma (user_id, name, idioma) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE idioma = VALUES(idioma)",
+                    (user_id, user_name, idioma)
                 )
                 connection.commit()
                 flag = next((f for f, c in emoji_flags.items() if c == idioma), None)
