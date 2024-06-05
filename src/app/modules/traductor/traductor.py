c520750b11a2a93da9ca5b487f501cee4c8f8e20
ArturoTF <arturotroyano@gmail.com>
2024-06-05 18:48:19 +0200

    Revert "fix"
    This reverts commit b4a20338a245c6431b16f3a3a3759ce135990239.

--- src/app/modules/traductor/traductor.py
+++ src/app/modules/traductor/traductor.py
@@ -5,11 +5,8 @@ from ...environments.utils import emoji_flags
 from ...environments.connection import create_connection, close_connection
 from ...environments.logging import safe_log
 
-
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.reactions = True
-intents.messages = True
-intents.guilds = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 translator = Translator()
